fadb4a838666a4e40d231277e6439568740c7698
Espeto <gkreisler5@hotmail.com>
2018-10-07 10:05:03 -0300

    Ultima atualização

--- filtro_kalman_calc.py
+++ filtro_kalman_calc.py
@@ -23,21 +23,46 @@ def calcula_variancia(media, medidas):
 def calcula_desvio_padrao(variancia):
 	
     return sqrt(variancia)
-	
+
+def ganho_kalman(variancia_cor, variancia_ultra):
+
+    ganho = variancia_cor**2 / (variancia_ultra**2 + variancia_cor**2)
+
+    return ganho
+
 def main():
     
-    media = 0.0
-    variancia = 0.0
-    desvio_padrao = 0.0
-    medidas = [99.0, 99.0, 98.1, 97.6, 98.9, 99.2, 100.3, 98.2, 98.7, 99.3, 100, 99.1, 99.4, 100.2, 100.3, 99.8, 98.7, 98.5, 100.0, 99.4, 98.6, 99.7, 99.5, 99.2, 99.0, 99.6, 99.4, 100.5, 98.9, 99.3]
-	
-    media = calcula_media(medidas)
-    variancia = calcula_variancia(media, medidas)
-    desvio_padrao = calcula_desvio_padrao(variancia)
+    media_ultra = 0.0
+    variancia_ultra = 0.0
+    desvio_padrao_ultra = 0.0
+    medidas_ultra = [99.0, 99.0, 98.1, 97.6, 98.9, 99.2, 100.3, 98.2, 98.7, 99.3, 100, 99.1, 99.4, 100.2, 100.3, 99.8, 98.7, 98.5, 100.0, 99.4, 98.6, 99.7, 99.5, 99.2, 99.0, 99.6, 99.4, 100.5, 98.9, 99.3]
+
+    media_cor = 0.0
+    variancia_cor = 0.0
+    desvio_padrao_cor = 0.0
+    medidas_cor = [104.0, 101.0, 103.4, 106.0, 105.5, 101.5, 108.4, 114.5, 104.4, 109.0, 117.8, 111.6, 105.5, 114.2, 102.0, 112.0, 101.9, 100.9, 103.5, 109.1, 103.8, 108.3, 105.3, 100.9, 100.3, 105.5, 98.7, 101.5, 103.6, 104.4]
+
+    media_ultra = calcula_media(medidas_ultra)
+    variancia_ultra = calcula_variancia(media_ultra, medidas_ultra)
+    desvio_padrao_ultra = calcula_desvio_padrao(variancia_ultra)
+
+    media_cor = calcula_media(medidas_cor)
+    variancia_cor = calcula_variancia(media_cor, medidas_cor)
+    desvio_padrao_cor = calcula_desvio_padrao(variancia_cor)
+
+    ganho = ganho_kalman(variancia_cor, variancia_ultra)
 	
     print("\n\n")
-    print("Media: {} \n".format(media))
-    print("Variancia: {} \n".format(variancia))
-    print("Desvio Padrao: {} \n".format(desvio_padrao))
+    print("Media Ultra Sensor: {} \n".format(media_ultra))
+    print("Variancia Ultra Sensor: {} \n".format(variancia_ultra))
+    print("Desvio Padrao Ultra Sensor: {} \n".format(desvio_padrao_ultra))
+
+    print("\n\n")
+    print("Media Color Sensor: {} \n".format(media_cor))
+    print("Variancia Color Sensor: {} \n".format(variancia_cor))
+    print("Desvio Padrao Color Sensor: {} \n".format(desvio_padrao_cor))
+
+    print("\n\n")
+    print("Ganho de Kalman: {}".format(ganho))
 	
 main()
--- src/kalman/Kalman.java
+++ src/kalman/Kalman.java
@@ -1,20 +1,40 @@
 package kalman;
 
 import lejos.nxt.*;
+import lejos.nxt.ColorSensor.Color;
+
+//1 pretinho = 1.05cm
 
 public class Kalman {
 	public static void main(String[] args) {
 		UltrasonicSensor usensor = new UltrasonicSensor(SensorPort.S1);
-		usensor.continuous();
-		int distance = 0;
+		ColorSensor colorSensor = new ColorSensor(SensorPort.S4);
+		int blackCounter = 0;
+		double filtroKalman = 0;
+		int distancia = 0;
+		int aux_kalman = 0;
 
-		while (distance < 101) {
-			distance = usensor.getDistance();
-			LCD.drawInt(distance, 0, 1);
-			Motor.A.setSpeed(100);
-			Motor.C.setSpeed(100);
+		while ( filtroKalman <= 100 ) {
+			
+			Motor.A.setSpeed(300);
+			Motor.C.setSpeed(300);
 			Motor.A.backward();
 			Motor.C.backward();
+			
+			distancia = usensor.getDistance();
+			
+			if (colorSensor.getColorID() == Color.BLACK) {
+				while(colorSensor.getColorID() == Color.BLACK);
+				blackCounter++;
+			}
+			
+			filtroKalman = (0.9995 * distancia) + ((1 - 0.995) * (blackCounter * 1.05));
+			
+			aux_kalman = (int) filtroKalman;
+			
+			LCD.drawInt(blackCounter, 0, 1);
+			LCD.drawInt(distancia, 0, 2);
+			LCD.drawInt(aux_kalman, 0, 3);
 		}
 	}
 
